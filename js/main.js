cd839ba5fe5d0412db96c8c06ed49c82e9aa2058
Frosenow <filip.potepa@gmail.com>
2022-09-06 14:34:12 +0200

    slider logic added

--- js/main.js
+++ js/main.js
@@ -1,17 +1,19 @@
-//The user will enter a cocktail. Get a cocktail name, photo, and instructions and place them in the DOM
 document.querySelector('button').addEventListener('click', getFetch)
 
 function getFetch(){
 
-const choice = document.querySelector('input').value.split(' ').join(' ')
-const url = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=' + choice
-fetch(url)
-.then(res => res.json()) // parse response as JSON
-.then(data => {
-  console.log(data.drinks[0])
-  document.querySelector('h2').innerText = data.drinks[0].strDrink
-  document.querySelector('img').src = data.drinks[0].strDrinkThumb
-  document.querySelector('h3').innerText = data.drinks[0].strInstructions
+  const choice = document.querySelector('input').value.split(' ').join(' ')
+  const url = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=' + choice
+  fetch(url)
+  .then(res => res.json()) // parse response as JSON
+  .then(data => {
+    const drinkImages = data.drinks.map(drink => {
+      let imgElement = document.createElement('img')
+      const imgUrl = drink.strDrinkThumb;
+      imgElement.src = imgUrl; 
+      document.querySelector('.slider').appendChild(imgElement)
+      
+    })
 })
 .catch(err => {
     console.log(`error ${err}`)
