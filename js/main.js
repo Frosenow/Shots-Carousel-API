64814bcb23dc517b0c9e7cfa4a2eb67328cb86f1
Frosenow <filip.potepa@gmail.com>
2022-09-06 16:54:21 +0200

    added method for adding divs containg name, image and instruction of making a drink

--- js/main.js
+++ js/main.js
@@ -1,6 +1,12 @@
 document.querySelector('button').addEventListener('click', getFetch)
+const sliderSection = document.querySelector('.slider')
+
+
 
 function getFetch(){
+  // Cleaning slider section by deleting old elements
+  let drinkDivs = document.querySelectorAll('.slider div')
+  drinkDivs.forEach(divChilde => sliderSection.removeChild(divChilde))
 
   const choice = document.querySelector('input').value.split(' ').join(' ')
   const url = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=' + choice
@@ -8,15 +14,30 @@ function getFetch(){
   .then(res => res.json()) // parse response as JSON
   .then(data => {
     const drinkImages = data.drinks.map(drink => {
-      let imgElement = document.createElement('img')
-      const imgUrl = drink.strDrinkThumb;
-      imgElement.src = imgUrl; 
-      document.querySelector('.slider').appendChild(imgElement)
+
+      const drinkSection = document.createElement('div'); 
       
+      const drinkName = drink.strDrink; 
+      const drinkHeader = document.createElement('h1')
+      drinkHeader.innerText = drinkName; 
+
+      const imgUrl = drink.strDrinkThumb;
+      const imgElement = document.createElement('img')
+      imgElement.src = imgUrl; 
+
+      const instructionText = drink.strInstructions; 
+      const instructionElement = document.createElement('p');
+      instructionElement.innerText = instructionText;
+
+      sliderSection.appendChild(drinkSection)
+      drinkSection.appendChild(drinkHeader)
+      drinkSection.appendChild(imgElement)
+      drinkSection.appendChild(instructionElement)
     })
 })
 .catch(err => {
-    console.log(`error ${err}`)
+    console.log(`error ${err}: Drink not found`)
+    alert('Drink not found')
 });
 }
 
